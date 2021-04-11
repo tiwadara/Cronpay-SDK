02469ceb18e897cfd8772cf05ebedaad69a9b723
Tiwalola <tiwababalola@gmail.com>
2021-04-11 07:58:47 +0100

    flutter sdk as aar

---
+++ cron_pay_flutter_sdk/.android/Flutter/build/generated/source/buildConfig/release/com/cronpay/cronpay_module/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.cronpay.cronpay_module;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String LIBRARY_PACKAGE_NAME = "com.cronpay.cronpay_module";
+  public static final String BUILD_TYPE = "release";
+}
--- cron_pay_flutter_sdk/android/app/src/main/kotlin/com/cronpay/cron_pay/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.cronpay.cron_pay
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
